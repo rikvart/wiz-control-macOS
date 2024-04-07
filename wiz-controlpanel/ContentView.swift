4f6a4664a049d89f72cd8e4ab1d547f390ae182d
rikvart <vartiainenriku@gmail.com>
2024-04-07 16:20:38 +0300

    Initial Commit

---
+++ wiz-controlpanel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wiz-controlpanel
+//
+//  Created by Riku Vartiainen on 7.4.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wiz-controlpanel/wiz_controlpanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  wiz_controlpanelApp.swift
+//  wiz-controlpanel
+//
+//  Created by Riku Vartiainen on 7.4.2024.
+//
+
+import SwiftUI
+
+@main
+struct wiz_controlpanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
